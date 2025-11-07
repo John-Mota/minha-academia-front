70cb78abe0bc6c6cbb10d11b6b186e548b1a2114
John Mota <johnmarcos.mota@gmail.com>
2025-11-07 14:40:26 -0300

    feat(mapa): Otimiza carregamento de tiles do mapa com CancellableNetworkTileProvider

--- lib/presentation/widgets/mapa/mapa.dart
+++ lib/presentation/widgets/mapa/mapa.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 
 class MapaWidget extends StatefulWidget {
   final Map<String, dynamic> geoJsonData;
@@ -250,7 +251,8 @@ class _MapaWidgetState extends State<MapaWidget> {
           TileLayer(
             urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
             userAgentPackageName: 'com.academia.fortaleza',
-            tileProvider: NetworkTileProvider(),
+            // Substituição para o provider otimizado
+            tileProvider: CancellableNetworkTileProvider(),
           ),
           if (_polygons.isNotEmpty) PolygonLayer(polygons: _polygons),
           if (_labels.isNotEmpty) MarkerLayer(markers: _labels),
